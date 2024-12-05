065d66c75998f35ffe6ec9a16964d6acf01dee5a
Tahsin-Sumaiyaa <stahsin@smith.edu>
2024-12-04 21:30:07 -0500

    guava new

---
+++ Room.java
@@ -0,0 +1,14 @@
+public class Room {
+    String name;
+    int serial;
+
+    public Room(String name, int serial){
+        this.name=name;
+        this.serial=serial;
+    } 
+
+    public String toString(){
+        return name +"("+serial+")";
+    }
+    
+}
